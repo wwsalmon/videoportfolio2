c2036cfee2b73c784488d117e530e154997ceb8b
wwsalmon <wwsamson@yahoo.com>
2018-06-06 22:52:54 -0400

    JS Sidebar, About, Contact pages

--- menu.js
+++ menu.js
@@ -1,12 +1,57 @@
-var clicked = false;
-$("body").prepend("<div class='topbar'><div class='topbar-button'><span>Menu</span></div></div>");
-$(".topbar-button").click(function(){
-  $(".sidebar").toggleClass("show-sidebar");
-  clicked = !clicked
-  if (clicked == true){
-    $(".topbar-button span").html("back");
+function buildMenu(page,select){
+  if (page == "home"){
+    $(".container").prepend(
+      `
+      <div class='topbar'><div class='topbar-button'><span>Menu</span></div></div>
+      <div class='sidebar'>
+        <div class='name'><span>samson<br/>zhang</span></div>
+        <div class='nav-item selected'><a href=''><span>Home</span></a></div>
+        <div class='nav-item'><a href='about/index.html'><span>About</span></a></div>
+        <div class='nav-item'><a href='szhang-resume.pdf'><span>Resume</span></a></div>
+        <div class='nav-item'><a href='contact/index.html'><span>Contact</span></a></div>
+        <div class='sidebar-social'>
+          <div class='sidebar-social-item yt'><a href='https://www.youtube.com/channel/UCNyoJQcV6NCHiRCeqcCxJag'><i class="fab fa-youtube"></i></a></div>
+          <div class='sidebar-social-item ig'><a href='https://www.instagram.com/samsonzhangthesalmon/'><i class="fab fa-instagram"></i></a></div>
+          <div class='sidebar-social-item tw'><a href='https://twitter.com/wwsalmon'><i class="fab fa-twitter"></i></a></div>
+        </div>
+      </div>
+      `
+    );
   }
   else{
-    $(".topbar-button span").html("menu");
+    $(".container").prepend(
+      `
+      <div class='topbar'><div class='topbar-button'><span>Menu</span></div></div>
+      <div class='sidebar'>
+        <div class='name'><span>samson<br/>zhang</span></div>
+        <div class='nav-item'><a href='../index.html'><span>Home</span></a></div>
+        <div class='nav-item about'><a href='../about/index.html'><span>About</span></a></div>
+        <div class='nav-item'><a href='../szhang-resume.pdf'><span>Resume</span></a></div>
+        <div class='nav-item contact'><a href='../contact/index.html'><span>Contact</span></a></div>
+        <div class='sidebar-social'>
+          <div class='sidebar-social-item yt'><a href='https://www.youtube.com/channel/UCNyoJQcV6NCHiRCeqcCxJag'><i class="fab fa-youtube"></i></a></div>
+          <div class='sidebar-social-item ig'><a href='https://www.instagram.com/samsonzhangthesalmon/'><i class="fab fa-instagram"></i></a></div>
+          <div class='sidebar-social-item tw'><a href='https://twitter.com/wwsalmon'><i class="fab fa-twitter"></i></a></div>
+        </div>
+      </div>
+      `
+    );
   }
-});
+  if (select == "about"){
+    $(".about").addClass("selected");
+  }
+  if (select == "contact"){
+    $(".contact").addClass("selected");
+  }
+  var clicked = false;
+  $(".topbar").click(function(){
+    $(".sidebar").toggleClass("show-sidebar");
+    clicked = !clicked
+    if (clicked == true){
+      $(".topbar-button span").html("back");
+    }
+    else{
+      $(".topbar-button span").html("menu");
+    }
+  });
+}
